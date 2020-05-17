a271697598f666750326f232500bbdd0879dd406
Grigory Shipunov <blame@oxapentane.com>
2020-05-17 02:36:06 +0200

    modules: mulvad: new default server

--- modules/wireguard-mullvad.nix
+++ modules/wireguard-mullvad.nix
@@ -1,15 +1,15 @@
 { config, ... }:
 
 {
-  networking.wg-quick.interfaces.mv-nl4 = {
-    address = [ "10.64.36.83/32" "fc00:bbbb:bbbb:bb01::1:2452/128" ];
+  networking.wg-quick.interfaces.mullvad-de4 = {
+    address = [ "10.66.12.213/32" "fc00:bbbb:bbbb:bb01::3:cd4/128" ];
     dns = [ "193.138.218.74" ];
-    privateKeyFile = "/etc/nixos/secrets/mv-nl4";
+    privateKeyFile = "/etc/nixos/secrets/mullvad";
     peers = [
       {
-        publicKey = "hnRyse6QxPPcZOoSwRsHUtK1W+APWXnIoaDTmH6JsHQ=";
+        publicKey = "XTdlx3YmLb1vg1NsruXYmfzP6FZKmZR90Vr3u9kJ7S0=";
         allowedIPs = [ "0.0.0.0/0" "::0/0" ];
-        endpoint = "185.65.134.224:51820";
+        endpoint = "185.213.155.130:51820";
       }
     ];
   };
