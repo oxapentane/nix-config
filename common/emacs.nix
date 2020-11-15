3d277b6a230bf2793270a8ff09002f9c6a2f895a
Grigory Shipunov <blame@oxapentane.com>
2020-11-16 00:23:32 +0100

    emacs.nix: refactor and switch to emacsGcc

--- common/emacs.nix
+++ common/emacs.nix
@@ -15,11 +15,20 @@
     lua53Packages.digestif # LaTeX
     clang-tools # C-family
     clang_10
+    # org requirements
+    gnuplot
+    sqlite
+  ];
+
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+    }))
   ];
 
   services.emacs = {
     enable = true;
     defaultEditor = true;
-    package = (import ./emacs-package.nix { inherit pkgs; });
+    package = with pkgs; ((emacsPackagesNgGen emacs).emacsWithPackages (epkgs: [ epkgs.vterm epkgs.pdf-tools ]));
   };
 }
